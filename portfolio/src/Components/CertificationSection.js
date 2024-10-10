ce039b017b8156760b36e74b4fa230f027633cac
isia-project <nicolas.massaia@bio.ens.psl.eu>
2024-10-10 20:02:03 +0200

    remove unused

--- portfolio/src/Components/CertificationSection.js
+++ portfolio/src/Components/CertificationSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAward, faBriefcase, faCertificate} from "@fortawesome/free-solid-svg-icons";
+import { faBriefcase} from "@fortawesome/free-solid-svg-icons";
 
 function CertificationSection() {
 
