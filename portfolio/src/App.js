39ffac5c6c2053423d7771ed83ee4f3481d8f488
isia-project <nicolas.massaia@bio.ens.psl.eu>
2024-10-14 19:19:22 +0200

    change html section to div

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Profil from "./Components/Profil";
 import MyStory from "./Components/MyStory";
@@ -11,95 +11,95 @@ import 'semantic-ui-css/semantic.min.css'
 import {Contact} from "./Components/Contact";
 
 function App() {
-  // Suivi de la position de la souris et du scroll
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [scrolling, setScrolling] = useState(false);
-  const [scrollTimeout, setScrollTimeout] = useState(null);
+    // Suivi de la position de la souris et du scroll
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    const [scrolling, setScrolling] = useState(false);
+    const [scrollTimeout, setScrollTimeout] = useState(null);
 
-  // Fonction pour mettre à jour la position de la lumière
-  const updateLightPosition = (clientX, clientY) => {
-    const scrollX = window.scrollX || 0;
-    const scrollY = window.scrollY || 0;
-    const newX = clientX + scrollX;
-    const newY = clientY + scrollY;
-    setMousePosition({ x: clientX + scrollX, y: clientY + scrollY });
-  };
-
-  // Mettre à jour la position de la souris en prenant en compte le scroll
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      updateLightPosition(e.clientX, e.clientY);
+    // Fonction pour mettre à jour la position de la lumière
+    const updateLightPosition = (clientX, clientY) => {
+        const scrollX = window.scrollX || 0;
+        const scrollY = window.scrollY || 0;
+        const newX = clientX + scrollX;
+        const newY = clientY + scrollY;
+        setMousePosition({x: clientX + scrollX, y: clientY + scrollY});
     };
 
-    const handleScroll = () => {
-      // Si un scroll est en cours, réinitialiser le timeout pour détecter la fin du scroll
-      if (scrollTimeout) {
-        clearTimeout(scrollTimeout);
-      }
-      setScrolling(true);
+    // Mettre à jour la position de la souris en prenant en compte le scroll
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            updateLightPosition(e.clientX, e.clientY);
+        };
 
-      // Démarrer un nouveau timeout qui n'exécute la fonction qu'à la fin du scroll
-      setScrollTimeout(setTimeout(() => {
-        setScrolling(false);
-        updateLightPosition(mousePosition.x - window.scrollX, mousePosition.y - window.scrollY);
-      }, 200)); // Délai de 200ms pour détecter la fin du scroll
+        const handleScroll = () => {
+            // Si un scroll est en cours, réinitialiser le timeout pour détecter la fin du scroll
+            if (scrollTimeout) {
+                clearTimeout(scrollTimeout);
+            }
+            setScrolling(true);
+
+            // Démarrer un nouveau timeout qui n'exécute la fonction qu'à la fin du scroll
+            setScrollTimeout(setTimeout(() => {
+                setScrolling(false);
+                updateLightPosition(mousePosition.x - window.scrollX, mousePosition.y - window.scrollY);
+            }, 200)); // Délai de 200ms pour détecter la fin du scroll
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [mousePosition, scrollTimeout]);
+
+    // Style pour l'effet de lumière suivant la souris
+    const lightStyle = {
+        position: 'fixed',
+        top: mousePosition.y - 10 + 'px',
+        left: mousePosition.x + 'px',
+        width: '300px',
+        height: '300px',
+        background: 'radial-gradient(circle, rgba(255,255,255,0.1) 10%, rgba(255,255,255,0.05) 40%, rgba(19,13,49,0) 80%)',
+        borderRadius: '50%',
+        pointerEvents: 'none',
+        mixBlendMode: 'screen',
+        transform: 'translate(-50%, -50%)',
     };
 
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('scroll', handleScroll);
+    return (
+        <div className="app">
+            {/* Lumière dynamique */}
+            <div style={lightStyle}></div>
 
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [mousePosition, scrollTimeout]);
+            {/* Section de gauche */}
+            <div className="left-section">
+                <Profil/>
+                <NavCustom/>
+                <Contact/>
+            </div>
 
-  // Style pour l'effet de lumière suivant la souris
-  const lightStyle = {
-    position: 'fixed',
-    top: mousePosition.y - 10 + 'px',
-    left: mousePosition.x + 'px',
-    width: '300px',
-    height: '300px',
-    background: 'radial-gradient(circle, rgba(255,255,255,0.1) 10%, rgba(255,255,255,0.05) 40%, rgba(19,13,49,0) 80%)',
-    borderRadius: '50%',
-    pointerEvents: 'none',
-    mixBlendMode: 'screen',
-    transform: 'translate(-50%, -50%)',
-  };
-
-  return (
-      <div className="app">
-        {/* Lumière dynamique */}
-        <div style={lightStyle}></div>
-
-        {/* Section de gauche */}
-        <div className="left-section">
-          <Profil />
-          <NavCustom />
-          <Contact />
+            {/* Section de droite */}
+            <div className="right-section">
+                <div id="about">
+                    <MyStory/>
+                </div>
+                <div id="experience">
+                    <ExperienceSection/>
+                </div>
+                <div id="skills">
+                    <SkillSection/>
+                </div>
+                <div id="certifications">
+                    <CertificationSection/>
+                </div>
+                <div className="works-section" id="works">
+                    <MyWorkSection/>
+                </div>
+            </div>
         </div>
-
-        {/* Section de droite */}
-        <div className="right-section">
-          <section id="about">
-            <MyStory />
-          </section>
-          <section id="experience">
-            <ExperienceSection />
-          </section>
-          <section id="skills">
-            <SkillSection />
-          </section>
-          <section id="certifications">
-            <CertificationSection />
-          </section>
-          <section className="works-section" id="works">
-            <MyWorkSection />
-          </section>
-        </div>
-      </div>
-  );
+    );
 }
 
 export default App;
